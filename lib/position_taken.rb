bac9a76b5b33bbf512d49c677525ee2f9627d6e7
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-10 16:42:49 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,8 @@
-# code your #position_taken? method here!
+def position_taken?(board,index)
+  val1 = board[index]
+  if val1 == " " || val1 == "" || val1 == nil
+    false
+  else
+    true
+  end
+end
